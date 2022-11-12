c13510feea9c1225be7fc0cfef6a2abb06c872bb
MuratcG <muratcangumuslu@gmail.com>
2022-11-12 03:19:58 +0100

    gets most of the basic stuff done for this project. Some basic css and the basic functions for the drawing boards

--- Etch-a-Sketch/app.js
+++ Etch-a-Sketch/app.js
@@ -1,12 +1,37 @@
-createGrid = () => {
-    for (let i = 0; i < 256; i++) {
-        const div = document.createElement('div');
-        div.classList.add('box');
-        div.addEventListener('mouseover', function(event){
-            event.target.style.backgroundColor = 'black';
-        })
-        drawingArea.appendChild(div); 
-    }
-    };
+const containerDiv = document.querySelector("#container");
 
-createGrid();
+function makeBoard(rows, columns) {
+
+    containerDiv.style.setProperty("--grid-rows", rows);
+    containerDiv.style.setProperty("--grid-columns", columns);
+    containerDiv.style.width = "650px";
+    containerDiv.style.height = "650px";
+    containerDiv.style.overflow = "hidden";
+    for (i = 0; i < (rows * columns); i++) {
+        let square = document.createElement("div");
+        square.style.minHeight = "0";
+        square.style.minWidth = "0";
+        square.style.overflow = "hidden";
+        containerDiv.appendChild(square).className = "grid-item";
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        })
+    } 
+}
+function resetBoard() {
+    const resetButton = document.querySelector("#resetButton");
+    resetButton.addEventListener("click", () => {
+        document.querySelectorAll(".grid-item").forEach(e => e.remove());
+        let userInput = prompt("Please enter the number of squares per side (biggest number allowed is 100 )");
+        if (userInput > 100) {
+            alert("Please choose a smaller number");
+            return;
+        }
+        rows = userInput;
+        columns = userInput;
+        makeBoard(rows, columns)
+    })
+}
+
+makeBoard(20, 20)
+resetBoard()
